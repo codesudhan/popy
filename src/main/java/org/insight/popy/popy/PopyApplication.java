c91bc9c9ab974303d2b7e59552f7c838c79a90a9
sudhanvag <sgnaneshwar@equinix.com>
2019-05-19 10:11:38 -0700

    Initial commit

---
+++ src/main/java/org/insight/popy/popy/PopyApplication.java
@@ -0,0 +1,13 @@
+package org.insight.popy.popy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/insight/popy/popy/PopyApplicationTests.java
@@ -0,0 +1,16 @@
+package org.insight.popy.popy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PopyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
